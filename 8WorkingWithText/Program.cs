2e8d1cde76c50d339f2509f3f0ee2f784bfacab8
Josh Piersimoni <68341491+JoshKP@users.noreply.github.com>
2020-11-16 11:15:59 -0500

    adjusted math

--- 8WorkingWithText/Program.cs
+++ 8WorkingWithText/Program.cs
@@ -10,38 +10,28 @@ namespace _8WorkingWithText
     {
         static void Main(string[] args)
         {
-            // 1- Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
-
-            Console.WriteLine("Enter several numbers separated by hyphens");
+            Console.WriteLine("Enter some numbers split by hyphens");
             string input = Console.ReadLine();
 
+            var stringArray = input.Split('-');
+            int[] numArray = Array.ConvertAll(stringArray, int.Parse);
 
-
-            List<string> inputList = new List<string>();
-            List<string> orderedList = new List<string>();
-            List<string> reverseList = new List<string>();
-
-            inputList = input.Split('-').ToList();
-            foreach (string number in inputList)
+            foreach (int n in numArray)
             {
-                inputList.Sort();
-                orderedList.Add(number);
+                int i = Array.IndexOf(numArray, n);
+                while (i <= numArray.Count()-1)
+                {
+                    if (numArray[i] + 1 == numArray[i + 1] || numArray[i] - 1 == numArray[i + 1])
+                    {
+                        i++;
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine("Consecutive");
+
+                }
             }
-            foreach (string number in orderedList)
-            {
-                orderedList.Reverse();
-                reverseList.Add(number);
-            }
-            if (inputList == orderedList || inputList == reverseList)
-            {
-                Console.WriteLine("Consecutive");
-            }
-            else
-                Console.WriteLine("Not Consecutive");
 
 
-
-           
         }
     }
 }
